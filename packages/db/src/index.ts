f217ca6b7e83329027622e2e134d9268b116958c
code-env <org.codeen@gmail.com>
2025-06-19 08:58:12 +0100

    refactor: update TypeScript configuration, modify import statements, and add initial SQL migration scripts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "../schema.js";
+import schema from "../schema.js";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
