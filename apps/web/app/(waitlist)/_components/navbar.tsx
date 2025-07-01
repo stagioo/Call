271cec006daa3fcb9c0ac8434483d5790d6de92c
Yassr atti <cloudec31@gmail.com>
2025-07-01 02:34:43 +0200

    feat: add demo button on waitlist

--- apps/web/app/(waitlist)/_components/navbar.tsx
+++ apps/web/app/(waitlist)/_components/navbar.tsx
@@ -43,6 +43,9 @@ const Navbar = () => {
           <span className="text-primary font-lora">Call</span>
         </h1>
         <div className="flex items-center gap-2">
+          <Button className=" bg-[#202020] text-white hover:bg-muted-foreground/10">
+            <Link href="/r">Demo</Link>
+          </Button>
           {links.map((link) => (
             <Button
               size="icon"
