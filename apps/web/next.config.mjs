2689604b4d3e720db29d6bfdf4762f28c79061fe
Yassr Atti <cloudec31@gmail.com>
2025-07-29 21:04:05 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@call/ui", "@call/auth"],
 };
 
 export default nextConfig;
