ebb74f7b0f0aea2ed4c7a114830d4aabe47d3f9a
Yassr atti <cloudec31@gmail.com>
2025-07-30 18:00:30 +0200

    feat: log in with email and password

--- apps/web/components/auth/social-button.tsx
+++ apps/web/components/auth/social-button.tsx
@@ -3,28 +3,14 @@
 import { authClient } from "@call/auth/auth-client";
 import { Button } from "@call/ui/components/button";
 import { Icons } from "@call/ui/components/icons";
-import { useState } from "react";
 
 const SocialButton = () => {
-  const [loading, setLoading] = useState(false);
-
   const handleGoogleLogin = async () => {
-    try {
-      setLoading(true);
-      console.log("Starting Google login...");
-      console.log("Using callback URL:", process.env.NEXT_PUBLIC_CALLBACK_URL);
-      
-      const { data } = await authClient.signIn.social({
-        provider: "google",
-        callbackURL: process.env.NEXT_PUBLIC_CALLBACK_URL,
-      });
-      
-      console.log("Login response:", data);
-    } catch (error) {
-      console.error("Google login error:", error);
-    } finally {
-      setLoading(false);
-    }
+    const { data } = await authClient.signIn.social({
+      provider: "google",
+      callbackURL: process.env.NEXT_PUBLIC_CALLBACK_URL,
+    });
+    console.log(data);
   };
 
   return (
@@ -33,10 +19,9 @@ const SocialButton = () => {
       variant="outline"
       size="lg"
       className="px-10!"
-      disabled={loading}
     >
-      {!loading && <Icons.google className="h-4 w-4" />}
-      {loading ? "Connecting..." : "Continue with Google"}
+      <Icons.google className="h-4 w-4" />
+      Continue with Google
     </Button>
   );
 };
