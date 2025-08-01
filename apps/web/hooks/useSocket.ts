af3b48f62d585d5943c5624d018e2a018f69037c
Yassr atti <cloudec31@gmail.com>
2025-08-01 12:48:36 +0200

    chore: just urls

--- apps/web/hooks/useSocket.ts
+++ apps/web/hooks/useSocket.ts
@@ -4,9 +4,12 @@ import { useEffect, useRef, useState } from "react";
 const getSocketUrl = () => {
   if (typeof window === "undefined") return "ws://localhost:4001";
   const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-  const host = "13.61.191.241";
-  const port = "4001";
-  return `${protocol}//${host}:${port}`;
+  const host = "media.joincall.co";
+  // const port = "4001";
+  if (process.env.NODE_ENV === "production") {
+    return `wss://media.joincall.co`
+  }
+  return `${protocol}//${host}`;
 };
 
 export function useSocket() {
