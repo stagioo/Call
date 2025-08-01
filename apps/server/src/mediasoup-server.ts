499ff01e0e1bd60e505c4ce739fbd62b3ebc813f
Yassr Atti <cloudec31@gmail.com>
2025-08-01 13:59:47 +0200

    Update mediasoup-server.ts

--- apps/server/src/mediasoup-server.ts
+++ apps/server/src/mediasoup-server.ts
@@ -109,7 +109,7 @@ const mediasoupConfig = {
   },
   webRtcTransport: {
 
-    listenIps: [{ ip: "127.0.0.1", announcedIp: "13.61.191.241" }],
+    listenIps: [{ ip: "0.0.0.0", announcedIp: "13.61.191.241" }],
 
     enableUdp: true,
     enableTcp: true,
