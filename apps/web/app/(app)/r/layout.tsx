cdd954b0370ec0c4343084ffbb70d36e8a671f89
code-env <org.codeen@gmail.com>
2025-08-02 11:17:23 +0100

    fix(demo): removed demo

--- apps/web/app/(app)/r/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const DemoLayout = ({ children }: { children: React.ReactNode }) => {
-  // Demo routes don't require authentication, render directly
-  return <>{children}</>;
-};
-
-export default DemoLayout; 
