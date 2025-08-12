d578e48693b57fee8181550e47fc91421b01a37d
code-env <org.codeen@gmail.com>
2025-08-12 10:20:58 +0100

    feat: fixed the join-call component

--- packages/auth/src/auth-client.ts
+++ packages/auth/src/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-console.log(process.env.BACKEND_URL);
-
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
