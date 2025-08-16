516b3aa29c9eb079625d68f7e919af10a6dfde65
code-env <org.codeen@gmail.com>
2025-08-16 20:59:15 +0100

    feat: removed react-scan from prod and livekit packages

--- apps/web/components/providers/theme-and-query.tsx
+++ apps/web/components/providers/theme-and-query.tsx
@@ -4,13 +4,22 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
+import { scan } from "react-scan";
+import { useEffect } from "react";
 
 export function ThemeAndQueryProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    scan({
+      enabled: process.env.NODE_ENV === "development",
+    });
+  }, []);
+
   const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <NextThemesProvider
       attribute="class"
